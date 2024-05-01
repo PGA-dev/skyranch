7abafa1ce1f1fe590b261c55a3ec47eb5d46bbef
PGA-dev <paulgadam@gmail.com>
2024-05-01 02:29:16 -0600

    updated pic

--- js/main.js
+++ js/main.js
@@ -8,7 +8,14 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 //
 // Place any custom JS here
 //
+// const myCarouselElement = document.querySelector('#fadeCarousel')
 
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   touch: false
+// })
+
+// document.querySelectorAll('[data-bs-slide]')
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach(popover => {
