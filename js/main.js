c3c78ae3b17c541e7f8907c6c8bab24035321d79
PGA-dev <paulgadam@gmail.com>
2024-05-09 22:54:56 -0600

    updated form join

--- js/main.js
+++ js/main.js
@@ -105,7 +105,27 @@ popoverTrigger.addEventListener('click', function () {
     .catch(error => console.error('Error loading the table row:', error));
 });
 
+// FORM
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
 
 
 //end of Callback nest for DomContentloaded
