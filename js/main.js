811db5b1004c174328311f54fd3f77227742172f
PGA-dev <paulgadam@gmail.com>
2024-07-05 17:28:26 -0600

    popover refactored to separate page

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@
 // Import the Bootstrap bundle
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import { initializePopover } from "./popover.js";
 
 //NEST
 document.addEventListener("DOMContentLoaded", function() {
   'use strict'
-
+  initializePopover();
+  console.log('DOM fully loaded and scripts initialized');
 
 
 
